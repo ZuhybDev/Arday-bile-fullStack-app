6f1bfb4b55022e0af69be1eb324e0ba219382f97
Zuhaib Mohammed <msuhaib637@gmail.com>
2026-02-11 22:16:31 +0300

    cross domain fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://arday-bile-fullstack-app.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
